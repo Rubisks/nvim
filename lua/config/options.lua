47134b629cc1c54755114356efa0089960fe55a3
tucker strand <tuckerstrand@tuckers-MacBook-Pro.local>
2025-09-06 19:05:39 -0600

    added C++ tools

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.diagnostic.config({
+  virtual_text = true,   -- shows inline errors like "expected ';'"
+  signs = true,          -- shows errors in the gutter
+  underline = true,      -- underlines problematic code
+  update_in_insert = false,
+})
